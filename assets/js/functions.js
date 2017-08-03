54c1c72411824361d7b11cc6acc19f217eefc3e6
Andrij Kozoriz <elgustos@gmail.com>
2017-08-03 19:10:13 +0300

    VC global page, about us page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -27,11 +27,34 @@
                 byRow: true
             });
         }
+        $('.fancybox').fancybox({padding: 0});
         setTimeout(equalfunc, 500);
-        // Changing the defaults
-        window.sr = ScrollReveal({ reset: true });
+        window.sr = ScrollReveal({reset: true});
+        sr.reveal('.anifade', {duration: 600});
 
-// Customizing a reveal set
-        sr.reveal('.anifade', { duration: 600 });
+        $joint_map = $('#vg');
+        $joint_map.mapster({
+            fillColor: 'b24a9a',
+            fillOpacity: 0,
+            showToolTip: true,
+            singleSelect: true,
+            toolTipContainer: '<div class="tooltipmapster"></div>',
+            onShowToolTip: function (e) {
+                var key_name = e.key;
+                var tooltip_text = $('#area_' + key_name).attr('data-tooltip');
+                e.toolTip.html(tooltip_text);
+            },
+            toolTipClose: ["tooltip-click", "area-click", "area-mouseout"],
+            mapKey: 'data-nid',
+            onClick: function (e) {
+                var fNid = $(this).attr('data-nid');
+                $(this).mapster('tooltip', fNid);
+            },
+            onMouseover: function (e) {
+                var fNid = $(this).attr('data-nid');
+                $joint_map.mapster('tooltip', fNid);
+            },
+        });
+
     });
 }(jQuery));
